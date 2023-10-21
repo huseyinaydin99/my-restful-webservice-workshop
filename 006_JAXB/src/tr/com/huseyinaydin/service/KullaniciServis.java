35e530c7080045ffa53ddf3bcc6b97f332fa0fe4
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-10-21 10:22:01 +0300

    RESTful Java XML Binding projesi yazıldı.
    JAXB(Java XML Binding) Java'dan XML'e XML'den ise Java'ya unmarshal yaptık.

---
+++ 006_JAXB/src/tr/com/huseyinaydin/service/KullaniciServis.java
@@ -0,0 +1,39 @@
+package tr.com.huseyinaydin.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import tr.com.huseyinaydin.model.Kullanici;
+
+//بسم الله الرحمن الرحيم
+
+/**
+* 
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java, Spring Boot.
+* 
+*/
+
+
+// http://localhost:8080/_006_JAXB/kullanici
+@Path ("/kullanici")
+public class KullaniciServis {
+
+	// http://localhost:8080/_006_JAXB/kullanici/detay/15
+	
+	@GET
+	@Path ("/detay/{id}")
+	@Produces (MediaType.APPLICATION_XML)
+	public Response getKullaniciBilgi(@PathParam("id") Integer id) {
+		Kullanici kullanici = new Kullanici();
+		kullanici.setKullaniciId(id);
+		kullanici.setKullaniciAdi("Hüseyin");
+		kullanici.setKullaniciSoyadi("Aydın");
+		return Response.status(200).entity(kullanici).build();
+	}
+}
